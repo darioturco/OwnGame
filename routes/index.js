a86a2309e8644519dcefc683bd35c06250b74a62
Dario <diort.2018@gmail.com>
2019-11-17 02:34:14 -0300

    Se mejoro el view principal y se dejo listo el view Overview

--- routes/index.js
+++ routes/index.js
@@ -2,88 +2,167 @@ require('dotenv').config();
 var express = require('express');
 var uni = require('./universe');
 var router = express.Router();
-var planeta = 0;
 
 console.log(new Date());
 router.all('/*', (req, res, next) => {
+  if(isFinite(req.query.planet)){
+    uni.planeta = parseInt(req.query.planet);
+  }
   uni.setPlayer(process.env.PLAYER, next);
 });
 
 /* GET pages. */
 router.get('/', (req, res, next) => {
+  //uni.deleteCollection(process.env.UNIVERSE_NAME, ["jugadores", "universo"]);
+  //uni.addNewPlayer("dturco", 1);
+
   uni.seeDataBase(res, process.env.UNIVERSE_NAME, process.env.JUGADORES);
 });
 
 router.get('/Highscore.html', (req, res, next) => {
-  res.render('Highscore', Object.assign({bodyId: "highscore"}, uni.getActualBasicInfo(planeta)));
+  res.render('Highscore', {bodyId: "highscore",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Defence.html', (req, res, next) => {
-  res.render('OGame_Defence', Object.assign({bodyId: "defense"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Defence', {bodyId: "defense",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: ['./Scripts/Description.js']
+  });
 });
 
 router.get('/OGame_Facilities.html', (req, res, next) => {
-  res.render('OGame_Facilities', Object.assign({bodyId: "station"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Facilities', {bodyId: "station",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: ['./Scripts/Description.js']
+  });
 });
 
 router.get('/OGame_Fleet.html', (req, res, next) => {
-  res.render('OGame_Fleet', Object.assign({bodyId: "fleet"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Fleet', {bodyId: "fleet",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Galaxy.html', (req, res, next) => {
-  res.render('OGame_Galaxy', Object.assign({bodyId: "galaxy"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Galaxy', {bodyId: "galaxy",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Messages.html', (req, res, next) => {
-  res.render('OGame_Messages', Object.assign({bodyId: "messages"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Messages', {bodyId: "messages",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Movement.html', (req, res, next) => {
-  res.render('OGame_Movement', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Movement', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Overview.html', (req, res, next) => {
-  res.render('OGame_Overview', Object.assign({bodyId: "overview"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Overview', {bodyId: "overview",
+    url: req._parsedOriginalUrl.pathname,
+    info: uni.overviewActualInfo(uni.planeta),
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Research.html', (req, res, next) => {
-  res.render('OGame_Research', Object.assign({bodyId: "research"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Research', {bodyId: "research",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: ['./Scripts/Description.js']
+  });
 });
 
 router.get('/OGame_Resources.html', (req, res, next) => {
-  res.render('OGame_Resources', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Resources', {bodyId: "resources",
+  url: req._parsedOriginalUrl.pathname,
+    info: uni.buildingsActualInfo(uni.planeta),
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: ['./Scripts/Description.js']
+  });
 });
 
 router.get('/OGame_ResourceSetings.html', (req, res, next) => {
-  res.render('OGame_ResourceSetings', Object.assign({bodyId: "resourceSettings"}, uni.getActualBasicInfo(planeta, "resourcesInitial(); ")));
+  res.render('OGame_ResourceSetings', {bodyId: "resourceSettings",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta, "resourcesInitial(); "),
+    listScript: ['./Scripts/Resources_Info.js']
+  });
 });
 
 router.get('/OGame_Reward.html', (req, res, next) => {
-  res.render('OGame_Reward', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Reward', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/OGame_Shipyard.html', (req, res, next) => {
-  res.render('OGame_Shipyard', Object.assign({bodyId: "shipyard"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Shipyard', {bodyId: "shipyard",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: ['./Scripts/Description.js']
+  });
 });
 
 router.get('/OGame_Tecnology.html', (req, res, next) => {
-  res.render('OGame_Tecnology', Object.assign({bodyId: "tecnology"}, uni.getActualBasicInfo(planeta)));
+  res.render('OGame_Tecnology', {bodyId: "tecnology",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/Options.html', (req, res, next) => {
-  res.render('Options', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('Options', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/Player.html', (req, res, next) => {
-  res.render('Player', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('Player', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/Search.html', (req, res, next) => {
-  res.render('Search', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('Search', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 router.get('/Vacas.html', (req, res, next) => {
-  res.render('Vacas', Object.assign({bodyId: "resources"}, uni.getActualBasicInfo(planeta)));
+  res.render('Vacas', {bodyId: "resources",
+    url: req._parsedOriginalUrl.pathname,
+    basic: uni.getActualBasicInfo(uni.planeta),
+    listScript: []
+  });
 });
 
 module.exports = router;
--- routes/universe.js
+++ routes/universe.js
@@ -4,23 +4,34 @@ require('mongodb').MongoClient.connect(process.env.MONGO_URL, {useUnifiedTopolog
   if(err) throw err;
   console.log('\x1b[32m%s\x1b[0m', "Base de datos lista.");
   mongo = db;
+  //carga los datos del universo
 });
-
-module.exports = {
-  getDate: function(dia) {
-    return 0;
+var exp = {
+  universo: {
+    name: "pepe",
+    inicio: 0,
+    maxGalaxies: 9,
+    donutGalaxy: true,
+    donutSystem: true,
+    speed: 1,
+    speedFleet: 1,
+    fleetDebris: 30,
+    defenceDebris: 0,
+    maxMoon: 20,
+    cantPlayers: 0
   },
   player: null,
+  planeta: 0,
   setPlayer: function(player, f) {
-    this.player = mongo.db(process.env.UNIVERSE_NAME).collection(process.env.JUGADORES).findOne({name: player}, (err, res) => {
+    mongo.db(process.env.UNIVERSE_NAME).collection(process.env.JUGADORES).findOne({name: player}, (err, res) => {
       if(err) throw err;
       this.player = res;
       //actualiza los datos desde la ultima conecccion
       f();
     });
   },
-  createNewPlanet: (cord, planetName, playerName) => {
-    var typePlanet = generateNewTypeOfPlanet(cord.pos, cord.system % 2);
+  createNewPlanet: function(cord, planetName, playerName) {
+    var typePlanet = this.generateNewTypeOfPlanet(cord.pos, cord.system % 2);
     return {idPlanet: Math.pow(500,2)*cord.galaxy + 500*cord.system + cord.pos,
       coordinates: cord,
       player: playerName,
@@ -33,21 +44,22 @@ module.exports = {
       resources: {metal: 500, crystal: 500, deuterium: 0, energy: 0},
       resourcesAdd: {metal: 20, crystal: 10, deuterium: 0},
       resourcesPercentage: {metal: 10, crystal: 10, deuterium: 10, energy: 10},
-      buildings: {metalMine: 0, crystalMine: 0, deuteriumMine: 0, solarPant: 0, fusionReactor: 0, metaltorage: 0, crystalStorage: 0, deuteriumStorage: 0, robotFactory: 0, shipyard: 0, researchLab: 0, alliance: 0, silo: 0, naniteFactory: 0, terraformer: 0},
+      buildings: {metalMine: 0, crystalMine: 0, deuteriumMine: 0, solarPlant: 0, fusionReactor: 0, metalStorage: 0, crystalStorage: 0, deuteriumStorage: 0, robotFactory: 0, shipyard: 0, researchLab: 0, alliance: 0, silo: 0, naniteFactory: 0, terraformer: 0},
       fleet: {lightFighter: 0, heavyFighter: 0, cruiser: 0, battleship: 0, battlecruiser: 0, bomber: 0, destroyer: 0, deathstar: 0, smallCargo: 0, largeCargo: 0, colony: 0, recycler: 0, espionageProbe: 0, solarSatellite: 0},
       defense: {rocketLauncher: 0, lightLaser: 0, heavyLaser: 0, gauss: 0, ion: 0, plasma: 0, smallShield: 0, largeShield: 0, antiballisticMissile: 0, interplanetaryMissile: 0},
       moon: {active: false},
       debris: {active:false, metal:0, crystal: 0}
-    }
+    };
   },
   addNewPlayer: (name, styleGame) => {
-    var cord = this.newCord();
-    var newPlanet = this.createNewPlanet(this.newCord(), "Planeta Principal", name);
+    var newPlanet = exp.createNewPlanet(exp.newCord(), "Planeta Principal", name);
+    var newPlanet1 = exp.createNewPlanet({galaxy: 1, system: 2, pos: 10}, "Colony1", name);
+    var newPlanet2 = exp.createNewPlanet({galaxy: 1, system: 3, pos: 12}, "Colony2", name);
     var newPlayer = {'name': name,
       'styleGame': styleGame,
-      planets: [newPlanet],
+      planets: [newPlanet, newPlanet1, newPlanet2],
       maxPlanets: 1,
-      highscore: this.getHigscoreNewPlayer(),//get la ultima position
+      highscore: exp.getHigscoreNewPlayer(),//get la ultima position
       puntos: 0,
       puntosAcum: 0,
       vacas: [],
@@ -60,6 +72,7 @@ module.exports = {
       lastVisit: 0,
       type: "activo"
     };
+    //aumenta en uno la cantidad de jugadores del universo
     mongo.db(process.env.UNIVERSE_NAME).collection("jugadores").insertOne(newPlayer);//agrega al jugador a la lista de jugadores
     mongo.db(process.env.UNIVERSE_NAME).collection("universo").insertOne(newPlanet);//agrega el planeta a la lista de planetas de manera ordenada por id
   },
@@ -79,25 +92,24 @@ module.exports = {
     if((mod == 0 && (pos == 12 || pos == 13)) || (mod == 1 && pos >= 14)) tipo = 5; // Gas
     if((mod == 0 && (pos == 10 || pos == 11)) || (mod == 1 && (pos == 12 || pos == 13))) tipo = 6; // Ice
     if(mod == 1 && (pos <= 3)) tipo = 7; // Desert
-    rango = Math.Random()*20+10;
+    rango = Math.floor(Math.random()*20+10);
     if(pos < 4){
-      temp = Math.floor(this.normalRandom(310-pos*50, 230-pos*50));// Cerca
+      temp = Math.floor(exp.normalRandom(310-pos*50, 230-pos*50));// Cerca
     }else{
       if(pos > 12){
-        temp = Math.floor(this.normalRandom(165-pos*50, 125-pos*50));// Lejos
+        temp = Math.floor(exp.normalRandom(165-pos*50, 125-pos*50));// Lejos
       }else{
-        temp = Math.floor(this.normalRandom(130-pos*10, 90-pos*10));// Medio
+        temp = Math.floor(exp.normalRandom(130-pos*10, 90-pos*10));// Medio
       }
     }
-    return {type: tipo, color: Math.floor(Math.random()*10)+1, temperature: {max: temp+rango, min: temp-rango}, campos: this.normalRandom(-0.022*Math.pow(pos,3)-0.73*Math.pow(pos,2)+17*pos+75, 0.056*Math.pow(pos,3)-3.12*Math.pow(pos,2)+36*pos+121)};
+    return {type: tipo, color: Math.floor(Math.random()*10)+1, temperature: {max: temp+rango, min: temp-rango}, campos: Math.floor(exp.normalRandom(-0.022*Math.pow(pos,3)-0.73*Math.pow(pos,2)+17*pos+75, 0.056*Math.pow(pos,3)-3.12*Math.pow(pos,2)+36*pos+121))};
   },
   getActualBasicInfo: function(planet, str="") {
-    var list = [];
+    let list = [];
     for(var i = 0 ; i<this.player.planets.length ; i++){
-      list.push({nombre: this.player.planets[i].name, coordinates: this.player.planets[i].coordinates, image: "./Imagenes/Planets/Planet_" + this.player.planets[i].type + ".png"});
+      list.push({name: this.player.planets[i].name, coordinates: this.player.planets[i].coordinates, type: this.player.planets[i].type, color: this.player.planets[i].color});
     }
-    var item = {
-      planets: list,
+    let item = {
       resources: this.player.planets[planet].resources,
       add: this.player.planets[planet].resourcesAdd,
       mesagges: this.player.messagesCant,
@@ -110,10 +122,25 @@ module.exports = {
       dark: this.player.dark,
       cantPlanets: this.player.planets.length,
       maxPlanets: this.player.maxPlanets,
-      planetName: this.player.planets[planet].name
+      numPlanet: planet,
+      planets: list
     };
   },
-  normalRandom: (min, max, podaMin = NaN, podaMax = NaN) => {// la esperanza es (max+min)/2
+  overviewActualInfo: function (planet) {
+    let cam = this.player.planets[planet].camposMax;
+    return {diameter: cam*66 + ' Km',
+      type: this.player.planets[planet].type,
+      temperature: this.player.planets[planet].temperature,
+      camposMax: cam,
+      campos: this.player.planets[planet].campos,
+      cantPlayers: this.universo.cantPlayers,
+      points: this.player.points
+    };
+  },
+  buildingsActualInfo: function (planet) {
+    return {buildings: this.player.planets[planet].buildings, solarSatellite: this.player.planets[planet].fleet.solarSatellite};
+  },
+  normalRandom: (min, max, podaMin = -Infinity, podaMax = Infinity) => {// la esperanza es (max+min)/2
     var u = 0, v = 0;
     while(u == 0) u = Math.random(); //Converting [0,1) to (0,1)
     let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v); // Box–Muller transform
@@ -121,12 +148,10 @@ module.exports = {
     if (num > 1 || num < 0) num = randn_bm(min, max, podaMin, podaMax); // resample between 0 and 1 if out of range
     num *= max - min; // Stretch to fill range
     num += min; // offset to min
-    if(!(isNaN(podaMin) || isNaN(podaMax))){
-      if (num > podaMax || num < podaMin) num = Math.random()*(podaMax-podaMin)+(podaMin);
-    }
+    if (num > podaMax || num < podaMin) num = Math.random()*(podaMax-podaMin)+(podaMin);
     return num;
   },
-  seeDataBase: function(res, uni, name) {
+  seeDataBase: (res, uni, name) => {
     var respuesta = "";
     var cursor = mongo.db(uni).collection(name).find();
     cursor.forEach((doc, err) => {
@@ -135,10 +160,14 @@ module.exports = {
       res.render('index', {title: 'Ogame', message: respuesta});
     });
   },
-  deleteRecord: function(id, uni, name){
+  deleteRecord: (id, uni, name) => {
     mongo.db(uni).collection(name).deleteOne({"_id": objectId(id)});
   },
-  deleteCollection: function(uni, name){
-    mongo.db(uni).dropCollection(name, (err, delOK) => {if(delOK) console.log('\x1b[35m%s\x1b[0m', "Collection" + name + "deleted")});
+  deleteCollection: (uni, nameList) => {
+    for(let i = 0 ; i<nameList.length ; i++){
+      mongo.db(uni).dropCollection(nameList[i], (err, delOK) => {if(delOK) console.log('\x1b[35m%s\x1b[0m', "Collection" + nameList[i] + "deleted")});
+    }
   }
-}
+};
+
+module.exports = exp;
